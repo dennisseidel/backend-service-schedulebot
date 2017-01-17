a5296390728f6fa26ae4a33b01a7f479ce98fbb9
Dennis Seidel <mail@dennisseidel.de>
2017-01-17 09:29:58 +0100

    feat: greet message on client connect

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 const server = Server(app);
 const io = socket(server);
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  io.emit('bot-message', {
+    type: 'bot',
+    message: 'Hi, what can I do for you?',
+  });
+});
+
 server.listen(port, (err) => {
   if (err) {
     console.log(`Error: ${err}`);
